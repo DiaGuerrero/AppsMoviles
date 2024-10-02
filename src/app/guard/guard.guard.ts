6f75d43d2d44c953345c96e79528a902d646b986
Juan Pablo <jpablo.valdebenitom@gmail.com>
2024-10-02 20:35:22 -0300

    se crea el servicio para resguardar las vistas

---
+++ src/app/guard/guard.guard.ts
@@ -0,0 +1,15 @@
+import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthserviceService } from '../service/authservice.service'; // importamos la clase recien creada llamada authservice
+import { Router } from '@angular/router';
+
+export const guardGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthserviceService);
+  const router = inject(Router)
+  
+  if(authService.isLoggedIn()){
+    return true;
+  }else{
+    return router.createUrlTree(['/login']);
+  }
+};
