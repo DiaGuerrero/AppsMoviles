bf28446949a15c743900c158479f28f9344fa6ea
Juan Pablo <jpablo.valdebenitom@gmail.com>
2024-10-02 20:31:28 -0300

    se modifica el app-routting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,45 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { guardGuard } from './guard/guard.guard';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule), 
+    canActivate: [guardGuard]   
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
-    path: 'profesor',
-    loadChildren: () => import('./profesor/profesor.module').then( m => m.ProfesorPageModule)
-  },
-  {
-    path: 'alumno',
-    loadChildren: () => import('./alumno/alumno.module').then( m => m.AlumnoPageModule)
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
   {
     path: 'cont-alumno',
-    loadChildren: () => import('./cont-alumno/cont-alumno.module').then( m => m.ContAlumnoPageModule)
+    loadChildren: () => import('./cont-alumno/cont-alumno.module').then( m => m.ContAlumnoPageModule),
+    canActivate: [guardGuard]
   },
   {
     path: 'cont-profesor',
-    loadChildren: () => import('./cont-profesor/cont-profesor.module').then( m => m.ContProfesorPageModule)
+    loadChildren: () => import('./cont-profesor/cont-profesor.module').then( m => m.ContProfesorPageModule),
+    canActivate: [guardGuard]
   },
   {
     path: 'qr-alumno',
-    loadChildren: () => import('./qr-alumno/qr-alumno.module').then( m => m.QrAlumnoPageModule)
+    loadChildren: () => import('./qr-alumno/qr-alumno.module').then( m => m.QrAlumnoPageModule),
+    canActivate: [guardGuard]
   },
   {
     path: 'qr-profesor',
-    loadChildren: () => import('./qr-profesor/qr-profesor.module').then( m => m.QrProfesorPageModule)
+    loadChildren: () => import('./qr-profesor/qr-profesor.module').then( m => m.QrProfesorPageModule),
+    canActivate: [guardGuard]
+  },
+  {
+    path: '**', // se agrega el doble * para que consulte todas las rutas, si no coincide vuelve al page
+    loadChildren: () => import('./page404/page404.module').then( m => m.Page404PageModule)
   },
 ];
 
